2b55f668568fd0cb1b7c3f1537ed5ad1f727fce4
Uzmakh <urbazoha@gmail.com>
2023-08-08 00:52:18 +0500

    Added axios library
    Building API Integration to the app

---
+++ src/index.js
@@ -0,0 +1,3 @@
+let apiKey = "f4e61697436f5bcdc88a3c3abb10odt1";
+let apiUrl = ` https://api.shecodes.io/weather/v1/current?query=Lisbon&key=f4e61697436f5bcdc88a3c3abb10odt1&units=metric`;
+console.log(apiUrl);
