211156ab665c830d33e60ec19afcfed3858be483
Uzmakh <urbazoha@gmail.com>
2023-08-08 21:13:56 +0500

    Styling temperature element
    Giving it a little bit position to the right

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-let apiKey = "f4e61697436f5bcdc88a3c3abb10odt1";
-let apiUrl = ` https://api.shecodes.io/weather/v1/current?query=Lisbon&key=f4e61697436f5bcdc88a3c3abb10odt1&units=metric`;
-console.log(apiUrl);
+function displayTemperature(response) {
+    console.log(response);
+    console.log(response.data.name);
+    console.log(response.data.main.temp);
+    console.log(response.data.weather[0].main);
+    console.log(response.data.weather[0].description);
+    console.log(response.data.main.humidity);
+    console.log(response.data.wind.speed);
+    let cityElement = document.querySelector("#city");
+    let temperatureElement = document.querySelector("#temperature");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+
+    cityElement.innerHTML = response.data.name;
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "8caba765ef85607a7196e7645c5e36e8";
+let apiUrl = ` https://api.openweathermap.org/data/2.5/weather?q=Dubai&appid=${apiKey}&units=metric`;
+// console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
