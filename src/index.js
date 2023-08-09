6c82a8199dbed8aca44c49fb3a75df8cf26dbd59
Uzmakh <urbazoha@gmail.com>
2023-08-09 23:46:28 +0500

    Added search engine

--- src/index.js
+++ src/index.js
@@ -42,8 +42,25 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "8caba765ef85607a7196e7645c5e36e8";
-let city = "Murree";
-let apiUrl = ` https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-// console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+    let apiKey = "8caba765ef85607a7196e7645c5e36e8";
+    // let city = "Murree";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+    // console.log(cityInputElement.value);
+}
+
+search("Multan");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+
+
